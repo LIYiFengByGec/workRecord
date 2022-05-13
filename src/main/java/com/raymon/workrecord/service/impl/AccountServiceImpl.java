754441b920e81e22e5b15753026f90ec028ad3d5
liyifeng <1367204445@qq.com>
2022-05-13 10:46:15 +0800

    增加密码加密功能，使用AES加密

--- src/main/java/com/raymon/workrecord/service/impl/AccountServiceImpl.java
+++ src/main/java/com/raymon/workrecord/service/impl/AccountServiceImpl.java
@@ -12,6 +12,7 @@ import com.raymon.workrecord.entity.Account;
 import com.raymon.workrecord.exception.BusinessException;
 import com.raymon.workrecord.pojo.LoginForm;
 import com.raymon.workrecord.service.AccountService;
+import com.raymon.workrecord.utils.EncryptAesUtil;
 
 @Service
 public class AccountServiceImpl implements AccountService{
@@ -26,7 +27,7 @@ public class AccountServiceImpl implements AccountService{
 		query.eq("username", username);
 		Integer integer = accountMapper.selectCount(query);
 		if(integer <= 0) {
-			account.setPassword(account.getPhoneNumber());
+			account.setPassword(EncryptAesUtil.aesEncrypt(account.getPhoneNumber()));
 			accountMapper.insert(account);
 		}else {
 			throw new BusinessException("该用户已存在");
@@ -41,7 +42,7 @@ public class AccountServiceImpl implements AccountService{
 		Account account = accountMapper.selectOne(query);
 		if(account == null) {
 			throw new BusinessException("用户名不存在");
-		}else if(!account.getPassword().equals(form.getPassword())){
+		}else if(!account.getPassword().equals(EncryptAesUtil.aesEncrypt(form.getPassword()))){
 			throw new BusinessException("密码错误");
 		}else {
 			return account;
